d776e9d14ebe1c9148633bad10e72b1ded594ca5
kajal <kajal3284@gmail.com>
2024-08-04 14:31:11 +0530

    updated

--- 06-Array.sh
+++ 06-Array.sh
@@ -4,7 +4,7 @@ Movies=["RRR","bhahubali","egga"]
 #index in list starts with 0
 #but size of the above array is 3.
 
-echo "First movie is:${$Movies[0]}"
-echo "Second movie is:${$Movies[1]}"
-echo "Third movie is:${$Movies[2]}"
+echo "First movie is:${Movies[0]}"
+echo "Second movie is:${Movies[1]}"
+echo "Third movie is:${Movies[2]}"
 echo "All movie are:${Movies[@]}"
