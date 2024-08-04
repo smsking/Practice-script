bde11292bb50be7e64ac45d6cf1b4505d41eee72
kajal <kajal3284@gmail.com>
2024-08-04 16:35:16 +0530

    updated

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,10 +3,10 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIP_TNAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
-if [$1 ne 0]
+if [ $1 -ne 0 ]
 then
     echo"$2 is a Failure"
     exit 1
@@ -15,7 +15,7 @@ else
 fi
 }
 
-if [$USERID ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "Run the Script with Root user"
     exit 1 #Manually exit if error comes.
@@ -26,7 +26,7 @@ fi
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "installing sql" 
 
-dnf install mysql -y &>>$LOGFILE
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "installing git" 
 
 
