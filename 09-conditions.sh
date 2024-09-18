87f16edd325ab34481accc6b7aaba880f027e857
saikumar <saikumarnallagatla@gmail.com>
2024-09-18 15:15:14 +0530

    Shell Scripting

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-NUMBER=$5
+# NUMBER=$5
 
-if [ $NUMBER -gt 10 ]
+# if [ $NUMBER -gt 10 ]
+# then
+#     echo "The Given $NUMBER is greater than 10"
+# else
+#     echo "The Given $NUMBER is  Lesser than 10"
+# fi
+
+# # -gt, -lt, -eq, -ge, -le
+
+sai=$8
+
+if [ $sai -le 20]
 then
-    echo "The Given $NUMBER is greater than 10"
+    echo "The Given $sai is lessthan 20"
 else
-    echo "The Given $NUMBER is  Lesser than 10"
+    echo "The Given $sai is greater than 20"
 fi
-
-# gt, lt, eq, ge, le
+ 
