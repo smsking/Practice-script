7ee6c98dc284d557402d4aa1d826892414ae9ba6
saikumar <saikumarnallagatla@gmail.com>
2024-09-18 15:07:26 +0530

    Terraform

--- 05-variables3.sh
+++ 05-variables3.sh
@@ -2,10 +2,10 @@
 
 echo "Please enter username:: "
 
-read -s USERNAME    # -s is for hide the username or password
+read  USERNAME    # -s is for hide the username or password
 
 echo "please enter password:: "
 
-read -s PASSWORD
+read  PASSWORD
 
 echo "Username is: $Username pasword is: $Password"
