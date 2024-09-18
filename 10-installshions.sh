22ffdcc96d990a4796aaaa0b1a171c20d31d8fc9
saikumar <saikumarnallagatla@gmail.com>
2024-09-18 16:00:50 +0530

    Shell Scripting

--- 10-installshions.sh
+++ 10-installshions.sh
@@ -5,7 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Run the Script with Root user"
-    #exit 1 #Manually exit if error comes.
+    exit 1 #Manually exit if error comes.
 else
     echo "Your super user."
 fi
@@ -17,22 +17,22 @@ dnf install mysql -y
 if [ $? -ne 0 ]
 then
     echo "MySQL installation is .....Failure"
-    #exit 1
+    exit 1
 else
     echo "MySQL installation is .....SUCCESS"
 fi
 
-# #installing git
+#installing git
 
-# dnf install git -y
+dnf install git -y
 
-# if [ $? -ne 0 ]
-# then
-#     echo "Git installation is .....Failure"
-#     exit 1
-# else
-#     echo "Git installation is .....SUCCESS"
-# fi
+if [ $? -ne 0 ]
+then
+    echo "Git installation is .....Failure"
+    exit 1
+else
+    echo "Git installation is .....SUCCESS"
+fi
 
 echo "is script Running?" 
 
