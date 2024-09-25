09c17dfcb1a35035b263d21675842bce3ad59d67
saikumar <saikumarnallagatla@gmail.com>
2024-09-25 20:01:36 +0530

    Shell scripting

--- 05-variables3.sh
+++ 05-variables3.sh
@@ -9,3 +9,31 @@ echo "please enter password:: "
 read -s PASSWORD
 
 echo "Username is: $Username pasword is: $Password"
+
+
+#this code for reference
+# Write a Bash script which accepts name as input and displays the greeting "Welcome (name)"
+
+#!/bin/bash
+# This is a comment. It won't be executed.
+
+# This line prompts the user to enter their name.
+read -p "Please enter your name: " name
+
+# This line prints the greeting to the console.
+echo "Welcome $name"
+
+# # Sure, I'd be happy to explain each element in the line 
+# read -p "Please enter your name: " name.
+
+# # read: This is a built-in bash command that reads input from the user.
+
+# # -p: This is an option flag that specifies a prompt message to display before reading the input. 
+# In this case, the prompt message is "Please enter your name: ".
+
+# # "name": This is the variable name that will store the input provided by the user. In this case, 
+# the input will be stored in the variable name.
+
+# # So, when you run the script and see the prompt "Please enter your name: ", 
+# you can type your name and press enter. The input will be stored in the name variable, 
+# which can then be used in the script
--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -6,3 +6,30 @@ No2=$2
 sum=$(($No1+$No2))
 
 echo "sum of $No1 and $No2 is:$sum"
+
+
+
+# #!/bin/bash
+
+# # Read the two integers from the user
+# read num1
+# read num2
+
+# read: This is a built-in command in Bash that is used to read input from the standard input (usually the keyboard).
+# num1: This is a variable where the input will be stored.
+# So, when you run this line, the program will pause and wait for you to enter a number.
+# Once you enter a number and press Enter,
+# that number will be stored in the variable num1 for later use.
+
+# # Calculate the sum, difference, product, and quotient
+# sum=$(($num1 + $num2))
+# difference=$(($num1 - $num2))
+# product=$(($num1 * $num2))
+# quotient=$(($num1 / $num2))
+
+# # Print the results   
+
+# echo "Sum: $sum"
+# echo "Difference: $difference"
+# echo "Product: $product"
+# echo "Quotient: $quotient"   
